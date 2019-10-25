f14d26d3f8983b116166b1627a1bbba7cacccdc8
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-10-25 12:38:07 +0530

    Added cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
@@ -6,10 +9,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _HomePage extends State<MyApp> {
+  List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 59.9,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Home Groceries',
+      amount: 29.2,
+      date: DateTime.now(),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Home Page"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              color: Colors.blue,
+              child: Text('Chart'),
+            ),
+            Card(
+              color: Colors.red,
+              child: Text('List of TX'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
